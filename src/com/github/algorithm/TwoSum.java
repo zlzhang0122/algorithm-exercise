75b87e0b5f8e62fef37ecfcf56bb2db12512be32
zlzhang0122 <zlzhang0122@gmail.com>
2019-12-01 21:37:20 +0800

    init and first

---
+++ src/com/github/algorithm/TwoSum.java
@@ -0,0 +1,33 @@
+package com.github.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: zlzhang0122
+ * @Date: 2019/12/1 9:26 PM
+ */
+public class TwoSum {
+    public static void main(String[] args){
+        TwoSum twoSum = new TwoSum();
+        int[] nums = new int[]{2, 7, 11, 15};
+        int[] res = twoSum.twoSum(nums, 9);
+
+        System.out.print(res[0] + "," + res[1]);
+    }
+
+    public int[] twoSum(int[] nums, int target){
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++){
+            int except = target - nums[i];
+            if(map.containsKey(except)){
+                res[0] = map.get(except);
+                res[1] = i;
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
