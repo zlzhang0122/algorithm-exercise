67125a8d1f38f74ae55b31f2ac21f8c391898541
zlzhang0122 <zlzhang0122@gmail.com>
2020-01-06 21:56:12 +0800

    stock two

--- src/com/github/algorithm/stocktrade/StockOne.java
+++ src/com/github/algorithm/stocktrade/StockOne.java
@@ -1,22 +1,30 @@
 package com.github.algorithm.stocktrade;
 
 /**
- * 只允许购买一次和卖出一次
- *
  * @Author: zlzhang0122
  * @Date: 2020/1/6 5:53 PM
  */
 public class StockOne {
     public static void main(String[] args){
-//        int[] arr = {7, 1, 5, 3, 6, 4};
-        int[] arr = {7, 6, 5, 4, 3, 2, 1};
-        int res = maxTradeSum(arr);
+        int[] arr = {7, 1, 5, 3, 6, 4};
+//        int[] arr = {7, 6, 5, 4, 3, 2, 1};
+//        int[] arr = {1, 2, 3, 4, 5};
 
-        System.out.println(res);
+        int res1 = maxTradeProfitOne(arr);
+        System.out.println(res1);
+
+        int res2 = maxTradeProfitTwo(arr);
+        System.out.println(res2);
     }
 
-    private static int maxTradeSum(int[] arr){
-        int min = Integer.MAX_VALUE, profile = 0;
+    /**
+     * 只允许购买一次和卖出一次
+     *
+     * @param arr
+     * @return
+     */
+    private static int maxTradeProfitOne(int[] arr){
+        int min = Integer.MAX_VALUE, profit = 0;
         if(arr == null){
             return 0;
         }
@@ -26,9 +34,27 @@ public class StockOne {
                 min = arr[i];
             }
 
-            profile = Math.max(arr[i] - min, profile);
+            profit = Math.max(arr[i] - min, profit);
         }
 
-        return profile;
+        return profit;
+    }
+
+    private static int maxTradeProfitTwo(int[] arr){
+        int min = Integer.MAX_VALUE, profit = 0;
+        if(arr == null){
+            return 0;
+        }
+
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }else{
+                profit = profit + arr[i] - min;
+                min = arr[i];
+            }
+        }
+
+        return profit;
     }
 }
