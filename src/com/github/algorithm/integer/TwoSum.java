a1186e2e07c76a141d54b249d435201f625b4de7
zlzhang0122 <zlzhang0122@gmail.com>
2021-01-17 23:48:08 +0800

    add two sum algorithm

--- src/com/github/algorithm/integer/TwoSum.java
+++ src/com/github/algorithm/integer/TwoSum.java
@@ -12,10 +12,12 @@ import java.util.Map;
 public class TwoSum {
     public static void main(String[] args){
         TwoSum twoSum = new TwoSum();
-        int[] nums = new int[]{2, 7, 11, 15};
+        int[] nums = new int[]{2, 7, 11, 15, 3, 6, 6, 3};
         int[] res = twoSum.twoSum(nums, 9);
 
-        System.out.print(res[0] + "," + res[1]);
+        System.out.println("find: " + res[0] + "," + res[1]);
+        //有重复数据时的情况
+        twoSum.twoSum2(nums, 9);
     }
 
     public int[] twoSum(int[] nums, int target){
@@ -26,10 +28,25 @@ public class TwoSum {
             if(map.containsKey(except)){
                 res[0] = map.get(except);
                 res[1] = i;
-            }else{
-                map.put(nums[i], i);
             }
+            map.put(nums[i], i);
         }
         return res;
+    }
+
+    public void twoSum2(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++){
+            int except = target - nums[i];
+            if(map.containsValue(except)){
+                for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+                    if(entry.getValue() == except){
+                        System.out.println("find: index:" + i + ",value:" + nums[i] +
+                                ", index:" + entry.getKey() + ",value:" + entry.getValue());
+                    }
+                }
+            }
+            map.put(i, nums[i]);
+        }
     }
 }
