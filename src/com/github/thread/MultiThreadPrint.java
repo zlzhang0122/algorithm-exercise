ba6b0f69d94304bced8718e5539628000c230f07
zlzhang0122 <zlzhang0122@gmail.com>
2021-06-29 00:10:39 +0800

    multi thread print

---
+++ src/com/github/thread/MultiThreadPrint.java
@@ -0,0 +1,34 @@
+package com.github.thread;
+
+/**
+ * 两个线程交替打印1到10
+ *
+ * @Author: zlzhang0122
+ * @Date: 2021/6/28 11:43 下午
+ */
+public class MultiThreadPrint {
+    public static volatile int i = 1;
+    public static void main(String[] args){
+        final Object o = new Object();
+
+        Runnable r = () -> {
+            synchronized (o){
+                for(;i<10;){
+                    System.out.println(Thread.currentThread().getName() + ": " + i++);
+                    try {
+                        o.notifyAll();
+                        o.wait();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    o.notifyAll();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(r, "thread-1");
+        Thread t2 = new Thread(r, "thread-2");
+        t2.start();
+        t1.start();
+    }
+}
